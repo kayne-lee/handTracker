e88baf50b3bc5782cfe47e5150e0632c5bb409d0
kayne-lee <kayne.lee2@outlook.com>
2024-05-06 22:50:36 -0400

    finish module

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,6 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -18,5 +20,10 @@ while True:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX,3, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
